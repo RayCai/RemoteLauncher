36ffcbb709f98344f687d3af1fd008e1ea1523e6
Ray.Cai <Ray.Cai@dextrys.com>
2013-06-07 17:29:30 +0800

    Implementing MainActivity for RemoteLauncherClient.

--- RemoteLauncerClient/src/com/ray/remotelauncerclient/MainActivity.java
+++ RemoteLauncerClient/src/com/ray/remotelauncerclient/MainActivity.java
@@ -1,22 +1,78 @@
 package com.ray.remotelauncerclient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+
+	private ViewPager viewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		viewPager = (ViewPager)findViewById(R.id.view_pager);
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setDisplayShowHomeEnabled(false);
+		actionBar.setDisplayShowTitleEnabled(false);
+		
+		MyTabListener tabListener = new MyTabListener();
+		Tab tab = actionBar.newTab().setText(R.string.apps).setTabListener(tabListener);
+		actionBar.addTab(tab);
+		tab = actionBar.newTab().setText(R.string.remote_control).setTabListener(tabListener);
+		actionBar.addTab(tab);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private class MyTabListener implements TabListener {
 
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			if (viewPager.getCurrentItem() != tab.getPosition())
+				viewPager.setCurrentItem(tab.getPosition());
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		}
+		
+	}
+	
+	private class MyPagerAdapter extends FragmentPagerAdapter {
+        List<Fragment> list = new ArrayList<Fragment>();
+        
+		public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+		
+		@Override
+		public Fragment getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
 }
